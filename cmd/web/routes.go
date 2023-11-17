cd49d63a81960c99ac30be55618ce65bf993aaaa
RistoFlink <kenssi@gmail.com>
2023-11-17 19:44:24 +0200

    Switch routing to chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,24 @@ import (
 
 	"github.com/RistoFlink/basic-web-app-go/pkg/config"
 	"github.com/RistoFlink/basic-web-app-go/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// using pat for routing
+	//mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	//using chi for routing
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
